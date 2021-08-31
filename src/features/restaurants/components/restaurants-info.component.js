e5979531d8fb93a4f8dde0e4f2f1007c85571b6b
Cristiano Alexandre de Faria <cristiano.alexandre@inatel.br>
2021-08-31 07:48:27 -0300

    class 109 add restaurant component

--- src/features/restaurants/components/restaurants-info.component.js
+++ src/features/restaurants/components/restaurants-info.component.js
@@ -1,6 +1,29 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, StyleSheet } from "react-native";
+import { Card } from "react-native-paper";
 
-export const RestaurantInfo = () => {
-  return <Text>Restaurant info.</Text>;
+export const RestaurantInfo = ({ restaurant = {} }) => {
+  const {
+    name = "Some restaurant",
+    icon,
+    photos = [
+      "https://i.pinimg.com/originals/6f/a6/27/6fa6279dd1ac18f07fcbdd7700aab14b.jpg",
+    ],
+    address,
+    isOpenNow = true,
+    rating = 4,
+    isCloseTemporarily,
+  } = restaurant;
+  return (
+    <Card elevation={5} style={styles.card}>
+      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+      <Text style={styles.title}>{name}</Text>
+    </Card>
+  );
 };
+
+const styles = StyleSheet.create({
+  card: { backgroundColor: "white" },
+  cover: { padding: 20, backgroundColor: "white" },
+  title: { padding: 16 },
+});
